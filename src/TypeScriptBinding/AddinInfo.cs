0999b35e1a20416be1fd7e03674bb3d249cc7535
chrisber <christian@chrisber.net>
2015-04-03 22:05:24 +0200

    update libraries

---
+++ src/TypeScriptBinding/AddinInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly:Addin ("TypeScript", 
+	Namespace = "MonoDevelop",
+	Version = MonoDevelop.BuildInfo.Version,
+	Category = "Language bindings")]
+
+[assembly:AddinName ("TypeScript Language Binding")]
+[assembly:AddinDescription ("Adds TypeScript support.")]
+
+[assembly:AddinDependency ("Core", MonoDevelop.BuildInfo.Version)]
+[assembly:AddinDependency ("Ide", MonoDevelop.BuildInfo.Version)]
+[assembly:AddinDependency ("Refactoring", MonoDevelop.BuildInfo.Version)]
+[assembly:AddinDependency ("SourceEditor2", MonoDevelop.BuildInfo.Version)]
+
